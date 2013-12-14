6638e560413d122727717c91759fe44ca236c06c
odbol <git@odbol.com>
2013-12-14 02:39:51 -0800

    Updated to work with manifest v2

---
+++ extension/background.js
@@ -0,0 +1,5 @@
+ chrome.browserAction.onClicked.addListener(function(tab) {
+                chrome.tabs.executeScript(null, {file:"cornify.js"}, function () {
+                    chrome.tabs.sendRequest(tab.id, 'cornify_page');
+                });
+            });

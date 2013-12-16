918bc7865fddac6d1d27748cb1addafaa622d583
odbol <git@odbol.com>
2013-12-16 02:20:47 -0800

    Removed Click to test sparkle sounds

--- extension/cornify.js
+++ extension/cornify.js
@@ -125,6 +125,7 @@
       });
     }
 
-    document.getElementsByTagName('body')[0].addEventListener("click", cornify, false);
+    // FOR TESTING:
+    //document.getElementsByTagName('body')[0].addEventListener("click", cornify, false);
   }
 })(window);

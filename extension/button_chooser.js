f952b16f346340c2fcd4113ab3c5a9af6b5dcab6
odbol <git@odbol.com>
2013-12-16 00:07:41 -0800

    Added MIDI popup form. Added underscore/backbone libs.

--- extension/button_chooser.js
+++ extension/button_chooser.js
@@ -2,6 +2,14 @@
 (function (exports) {
   
 	if (!exports.buttonChooser) {
+		/*** TEMPLATES ****/
+		var templateMIDI = '<div class="drumpants_midiForm">' +
+							'<label>Type or play a note:</label>' + 
+						   '<input type="text" class="midiNote" name="drumpants_midiNote" value="{{midiNote}}" placeholder="Midi Note" />' +
+						   '<span class="delete button">Clear</span>' +
+						   '</div>';
+
+
 		var animateElement = function (el) {
 				var $el = jQuery(el);
 
@@ -13,48 +21,126 @@
 					$el.removeClass("drumpants_buttonHit");
 				}, 500); 
 			},
+
+			// key : MIDI Note pitch
+			// value: HTML element to click
+			elementForNote = {},
+
+			removeNoteForElement = function (element) {
+				// remove old mapping
+				elementForNote = _.reject(elementForNote, function (el) {
+					return el == element;
+				});
+			},
+
+			// the DOM outliner
+			myDomOutline,
+
+			onMidiNote = function (pitch, vel) {
+				var element = elementForNote[pitch] || elementForNote["all"];
+
+				if (element) {
+					element.click();
+
+					animateElement(element);
+				}
+			},
+
+
+			// shows a form to type a note on the element
+			showLearnForm = function(element, sucesss) {
+				var formEl = $(templateMIDI),
+					closeForm = function () {
+						formEl.remove();
+
+						// Stop outline (also stopped on escape/backspace/delete keys):
+						myDomOutline.stop();
+					},
+					overlayElements,
+					box,
+					pos;
+
+				myDomOutline.pause();
+
+				overlayElements = myDomOutline.showBox().getElements();
+				box = overlayElements.bottom;
+				pos = box.position();
+				//box.empty()
+					// enable clickin on the child elements!
+					//.css('pointerEvents', 'auto');
+
+				formEl
+					.appendTo('body')
+					.css({
+		                top: pos.top + box.height(),
+		                left: pos.left
+		            })
+					.on('click', '.delete', function () {
+						box.find('.midiNote').val('');
+
+						removeNoteForElement(element);
+
+						closeForm();
+					})
+					.on('change', '.midiNote', function () {
+						var midiNoteElement = this,
+							note = this.value;
+
+						removeNoteForElement(element);
+
+						if (note) {
+							elementForNote[note] = element;
+						}
+
+						closeForm();
+					});
+			},
 			onChoseButton = function (element) { 
 				console.log('Clicked element:', element); 
 
-			// test animation by clicking anywhere
-			// $("body").click(function () {
-			// 	animateElement(element);
-			// });
 
-				var midiNote = prompt("Enter MIDI note to control this button: ", "*");
-				if (midiNote) {
+				showLearnForm(element, function (midiNote) {
+						// if (midiNote) {
 
-					// TODO: add MIDI note on listener to click the element
-					midicorn({
+						// 	// TODO: add MIDI note on listener to click the element
+						// 	midicorn({
 
-						noteOn: function (pitch, vel) {
-							
-							if (midiNote == "*" || pitch == midiNote) {
-								element.click();
+						// 		noteOn: function (pitch, vel) {
+									
+						// 			if (midiNote == "*" || pitch == midiNote) {
+						// 				element.click();
 
-								animateElement(element);
-							}
-						}
+						// 				animateElement(element);
+						// 			}
+						// 		}
+						// 	});
+
+						// 	// TODO: keep track of noteOn callbacks and clean them up when done!
+						// }
 					});
 
-					// TODO: keep track of noteOn callbacks and clean them up when done!
-				}
-				
-				// Stop outline (also stopped on escape/backspace/delete keys):
-				myDomOutline.stop();
 			},
-			myDomOutline = DomOutline({ 
-				onClick: onChoseButton,
-				border: 2,
-				realtime: true
-			}),
 			choose = function () {
 
 				// Start outline:
 				myDomOutline.start();
 
-				
+				// TODO: add MIDI note on listener to click the element
+				midicorn({
+					noteOn: onMidiNote
+				});
 			};
+
+		myDomOutline = DomOutline({ 
+			onClick: onChoseButton,
+			border: 2,
+			realtime: true
+		});
+
+		// test animation by clicking anywhere
+		// $("body").click(function () {
+		// 	onMidiNote(60, 127);
+		// });
 
 		exports.buttonChooser = {
 			choose: choose
